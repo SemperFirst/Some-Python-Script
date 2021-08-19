01e12722d3f96d16254ea959cbb0b75c63d791b2
Semperfi-ZX <56592386+Semperfi-ZX@users.noreply.github.com>
2021-08-19 22:34:38 +0800

    Update two_sum.py

--- two_sum.py
+++ two_sum.py
@@ -14,6 +14,16 @@ def two_sum(nums,target):
         return [i,j]
     else:
         return []
+    
+#字典模拟哈希表
+def two_sum(self, nums: List[int], target: int) -> List[int]:
+        hashmap={}
+        for ind,num in enumerate(nums):
+            hashmap[num] = ind
+        for i,num in enumerate(nums):
+            j = hashmap.get(target - num)
+            if j is not None and i!=j:
+                return [i,j]
 
 
 
